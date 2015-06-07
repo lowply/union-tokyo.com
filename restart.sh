4c6ae936d2677c329398f7db21bb66e49672f19a
Sho Mizutani <lowply@gmail.com>
2015-06-07 11:34:20 +0900

    update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-docker build -t lowply/union-tokyo.com .
-docker stop union-tokyo.com
-docker rm union-tokyo.com
-docker run -d --name union-tokyo.com --publish 3000:3000 lowply/union-tokyo.com
+cd $(dirname $0)
+
+logger(){
+	echo "$(date): ${1}" | tee -a logs/restart.log
+}
+
+type docker > /dev/null 2>&1 || { logger "docker is not installed"; exit 1; }
+type git > /dev/null 2>&1 || { logger "git is not installed"; exit 1; }
+
+USERNAME="lowply"
+APPNAME="union-tokyo.com"
+CPORT="3000"
+HPORT="3000"
+
+logger "---"
+logger "restart.sh kicked by webhook"
+
+logger "git pull ..."
+git pull
+
+logger "docker build ..."
+docker build -t ${USERNAME}/${APPNAME} .
+
+logger "restarting container ..."
+docker stop ${APPNAME}
+docker rm ${APPNAME}
+docker run -d --name ${APPNAME} -p ${HPORT}:${CPORT} ${USERNAME}/${APPNAME}
+
+logger "done"
